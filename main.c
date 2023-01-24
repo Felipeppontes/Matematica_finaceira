315ceb7a1517422da8f6f30832a7f5f924588996
Felipe Pontes <107514967+Felipeppontes@users.noreply.github.com>
2023-01-23 22:11:47 -0300

    salvando código original

--- main.c
+++ main.c
@@ -1,80 +1,58 @@
-//**********MÉDIA DAS NOTAS-SIMPLES***********
+//PROGRAMINHA PARA CALCULAR JUROS SIMPLES E COMPOSTOS, QUE ESTOU APREENDENDO ESTUDANDO MATEMÁTICA FINANCEIRA EM 01/2023
 
-//MÉDIAS DAS MINHAS NOTAS NA UNIVERSIDADE
 
 #include <stdio.h>
 
 int main(){
-    float  p1,p2,p3,tpa1,tpa2,tpa3,atv1,atv2,atv3,m_t1,m_t2,m_t3,m_a1,m_a2,m_a3,u1,u2,u3,media_f,nota_pf,media_pf;
+  float cap, taxa, tempo, mont,js , jc;
+  int opc;
+  char nome[100];
+  printf("Olá Sr! Primeiramente gostaria de saber qual tipo de juros você quer calcular...\n\nSe for simples digite 1 | montante 2 | Ambos digite 0\n");
+  scanf("%i", &opc);
 
-    //PROVAS E.E 1,2 E 3:
-    /*printf("Opa, Me informe seu nome por favor... \n\n\n\n");*/
-    printf("\n Por favor, digite suas 3 notas em ordem das unidades:\n\nPrimeira Unidade:");
-    scanf("%f", &p1);
-    printf("\nSegunda Unidade:");
-    scanf("%f", &p2);
-    printf("\nTerceira Unidade:");
-    scanf("%f", &p3);
+  
+  switch(opc){
+    case 1: //Calcular apenas o juros simples
 
-    //TPAs das 3 unidades:
-    printf("\n Por favor, digite suas notas dos TPAs desse periodo:\n\nPrimeira Unidade:");
-    scanf("%f", &tpa1);
-    printf("\nSegunda Unidade:");
-    scanf("%f", &tpa2);
-    printf("\nTerceira Unidade:");
-    scanf("%f", &tpa3);
-//media tpa
-    m_t1 = (1.5*tpa1)/10;
-    m_t2 = (1.5*tpa2)/10;
-    m_t3 = (1.5*tpa3)/10;
+  printf("\nÓtimo, vamos calcular o juros simples! Agora me informe os seguintes dados:\nNome:");
+  scanf("%s", &nome);
+  printf("\nCapital investido:");
+  scanf("%f", &cap );
+  printf("\nTaxa de rendimento (em  porcentagem):");
+  scanf("%f", &taxa);
+  printf("\nTempo de investimento:");
+  scanf("%f", &tempo);
+  printf(" %s, esse é o juros obtido com base no capital de R$:%.2f a uma taxa de %.2f em %.2f meses do ano: %.2f", nome, cap, taxa, tempo, cap * (taxa)/100 * tempo);
 
-    //ATIVIDADE DE AULA das 3 unidades:
-    printf("\n Por favor, digite suas notas dos exercicios desse periodo:\n\nPrimeira Unidade:");
-    scanf("%f", &atv1);
-    printf("\nSegunda Unidade:");
-    scanf("%f", &atv2);
-    printf("\nTerceira Unidade:");
-    scanf("%f", &atv3);
-//media atv
-    m_a1 = (1.5*atv1)/10;
-    m_a2 = (1.5*atv2)/10;
-    m_a3 = (1.5*atv3)/10;
+    case 2://calcular o montante
 
-    //Media das unidades
+    printf("\nÓtimo, Vamos calcular o montante! Agora me informe os seguintes dados:\nNome:");
+  scanf("%s", &nome);
+  printf("\nCapital investido:");
+  scanf("%f", &cap );
+  printf("\nTaxa de rendimento (em  porcentagem):");
+  scanf("%f", &taxa);
+  printf("\nTempo de investimento:");
+  scanf("%f", &tempo);
+  printf(" %s, esse é o montante obtido com base no capital de R$:%.2f a uma taxa de %.2f em %.2f meses do ano: %.2f", nome, cap, taxa, tempo, cap*(1+(taxa/100)*tempo));
 
-    u1=(m_t1+p1+m_a1);
-    u2=(m_t2+p2+m_a2);
-    u3=(m_t3+p3+m_a3);
-    
-    //Média final 
-    media_f=(u1+u2+u3)/3;
+    case 0://realizar os 2 calculos 
 
-    if (media_f >= 7.00)// aprovado caso > 7.00
-        {
-        printf("\nEsta aprovado!");
-        printf("\nA média das suas três notas foi: %.2f", media_f);
-        }
-    else if (media_f < 7.00 && media_f >= 3)//Final se entre 7 e 3
-    { 
-        printf("\n\nUfa! Você vai ter uma ultima chance, sua nota final ficou %.2f. Prova Final!\n", media_f);
-        printf("\n*Digite a nota da prova final: ");
-        scanf("%f", &nota_pf);
-      //média da prova final
-    media_pf = (media_f + nota_pf)/2;
-      
-        if (media_pf >= 5)// Se nota da prova final >=5
-        {
-            printf("\nVocê Conseguiu se recuperar, e ficou com a média final :\n\n %.2f", media_pf);
-        }
-        else //Se nota da prova final não >=5 
-        {
-            printf("Infelizmente não deu. Sua média após a final foi: %.2f", media_pf);
-        }
-    }//Caso media final seja <3
-    else
-        {
-        printf("\nLamento mas você está reprovado(a)!");
-        printf("\nA média das suas três notas foi: %.2f", media_f);
-        }
-    return 0;
+    printf("\nÓtimo, Vamos calcular o montante! Agora me informe os seguintes dados:\nNome:");
+  scanf("%s", &nome);
+  printf("\nCapital investido:");
+  scanf("%f", &cap );
+  printf("\nTaxa de rendimento (em  porcentagem):");
+  scanf("%f", &taxa);
+  printf("\nTempo de investimento:");
+  scanf("%f", &tempo);
+
+//juros
+  printf("\n %s, esse resultado foi obtido com base no capital de R$:%.2f a uma taxa de %.2f(porcento a.m) em %.2f meses do ano:\n\nJUROS******** %.2f *******", nome, cap, taxa, tempo, cap * (taxa)/100 * tempo);
+
+//montante  
+  printf("\n\nMONTANTE***** %.2f ******", cap*(1+(taxa/100)*tempo));
+     
+}
+  return 0;
 }
